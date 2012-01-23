f73b670bcb10c81d7ec324753956e6c772ea3f46
root <root@server>
2012-01-23 06:07:35 +0400

    update database schema use postgres random index for auth token

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,9 @@
--- /*
---
---    keyescrow: web application for securely escrowing keys
---    - schema.sql: database schema
---
---    Copyright (C) 2008-2011 Scot-Irish Lads, LLC
---    Author: Dustin Kirkland <dustin.kirkland@gmail.com>
---
---    This program is free software: you can redistribute it and/or modify
---    it under the terms of the GNU Affero General Public License as
---    published by the Free Software Foundation, version 3 of the
---    License.
---
---    This program is distributed in the hope that it will be useful,
---    but WITHOUT ANY WARRANTY; without even the implied warranty of
---    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---    GNU Affero General Public License for more details.
---
---    You should have received a copy of the GNU Affero General Public License
---    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
--- */
-
-
 --
 -- PostgreSQL database dump
 --
 
-SET client_encoding = 'SQL_ASCII';
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
 SET standard_conforming_strings = off;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
@@ -43,9 +20,8 @@ SET default_with_oids = false;
 --
 
 CREATE TABLE download (
-    key text NOT NULL,
-    expiration timestamp with time zone DEFAULT (now() + '00:15:00'::interval),
-    payload_id integer NOT NULL
+    payload_id bigint NOT NULL,
+    creation timestamp with time zone DEFAULT now() NOT NULL
 );
 
 
@@ -56,9 +32,10 @@ ALTER TABLE public.download OWNER TO postgres;
 --
 
 CREATE TABLE payload (
-    id integer DEFAULT nextval(('"payload_id_seq"'::text)::regclass) NOT NULL,
+    id bigint DEFAULT round(((9223372036854775807::bigint)::double precision * random())) NOT NULL,
     creation timestamp with time zone DEFAULT now() NOT NULL,
-    expiration timestamp with time zone DEFAULT (now() + '1 year'::interval),
+    registration timestamp with time zone,
+    expiration timestamp with time zone,
     note text,
     payload text NOT NULL,
     email text,
@@ -66,37 +43,27 @@ CREATE TABLE payload (
     filesize integer NOT NULL,
     md5 text NOT NULL,
     sha1 text NOT NULL,
-    auth text
+    auth bigint DEFAULT round(((9223372036854775807::bigint)::double precision * random())) NOT NULL
 );
 
 
 ALTER TABLE public.payload OWNER TO postgres;
 
 --
--- Name: payload_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: payload_id_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
-CREATE SEQUENCE payload_id_seq
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
+ALTER TABLE ONLY payload
+    ADD CONSTRAINT payload_id_pkey UNIQUE (id);
 
-
-ALTER TABLE public.payload_id_seq OWNER TO postgres;
 
 --
--- Name: retrieval; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+-- Name: payload_id_chk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-CREATE TABLE retrieval (
-    email text NOT NULL,
-    key text NOT NULL,
-    expiration timestamp with time zone DEFAULT (now() + '00:15:00'::interval)
-);
+ALTER TABLE ONLY download
+    ADD CONSTRAINT payload_id_chk FOREIGN KEY (payload_id) REFERENCES payload(id);
 
-
-ALTER TABLE public.retrieval OWNER TO postgres;
 
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
